f0197aa177f253a104c88c10e6ee974d4c643212
Gabriel Minatto <gabriel.minatto@outlook.com>
2019-07-14 21:02:30 -0300

    PasswordUtils.

---
+++ src/main/java/udemy/spring/electronicpoint/api/utils/PasswordUtils.java
@@ -0,0 +1,31 @@
+package udemy.spring.electronicpoint.api.utils;
+
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+public class PasswordUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+
+    /**
+     * Gera um hash utilizando o BCrypt.
+     *
+     * @param senha
+     * @return String
+     */
+    public static String gerarBCrypt(String senha) {
+        if (Objects.isNull(senha)) {
+            return senha;
+        }
+
+        log.info("Gerando hash com o BCrypt.");
+        BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+        return bCryptEncoder.encode(senha);
+    }
+
+}
